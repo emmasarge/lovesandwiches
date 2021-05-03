f5bed1401a0763b6a45f00f7edabb8d72c23199a
emmasarge <emmasargeant206@gmail.com>
2021-05-03 14:09:41 +0000

    made a while statement for the get sales data

--- run.py
+++ run.py
@@ -5,25 +5,33 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     GET SALES FIGURES FROM DATA
     """
-    print("Please enter sales data from the last market")
-    print("Data should be 6 figures, separated by commas.")
-    print("Example: 23,32,23,87,98,64\n")
+    while True:
+        print("Please enter sales data from the last market")
+        print("Data should be 6 figures, separated by commas.")
+        print("Example: 23,32,23,87,98,64\n")
 
-    data_str = input("Enter your data here:")
-    
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
 
 def validate_data(values):
     try:
@@ -31,8 +39,12 @@ def validate_data(values):
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you entered {len(values)}."
-        )
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again. \n")
+        return False
+    
+    return True
 
-get_sales_data()
+
+data = get_sales_data()
